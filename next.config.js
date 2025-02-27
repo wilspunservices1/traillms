4cfc97f885778e377a5cdc7255349e756fbbfcc9
wilspunservices1 <wilspunservicesgithub1@gmail.com>
2025-02-28 00:52:09 +0530

    Create next.config.js by JC

---
+++ next.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        pathname: "/ddj5gisb3/image/upload/**",
+      },
+    ],
+  },
+};
+
+module.exports = nextConfig;
